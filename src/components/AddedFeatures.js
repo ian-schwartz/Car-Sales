89bd6463e769e2bbba52acff43bfc5fc1765b089
Ian Schwartz <ian.schwartz23@gmail.com>
2019-10-09 15:58:36 -0700

    add feature

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import AddedFeature from './AddedFeature';
 
+
 const AddedFeatures = props => {
   return (
     <div className="content">
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
+
+
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={ props.clicked }>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
+
 
 export default AdditionalFeature;
